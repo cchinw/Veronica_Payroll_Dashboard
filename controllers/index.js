9caff100f78b49fddca0f4f9a334306808ed9889
Chinwendu <chinwenduukoha@gmail.com>
2022-06-18 19:41:34 -0400

    Added .env to gitignore

--- controllers/index.js
+++ controllers/index.js
@@ -1,1 +1,27 @@
 const { Employee } = require('../models/index')
+
+const createEmployee = async (req, res) => {
+  try {
+    const employee = await new Employee(req.body)
+    await employee.save()
+    return res.status(201).json({ employee })
+  } catch (error) {
+    return res.status(500).json({ error: error.message })
+  }
+}
+
+const getEmployees = async (req, res) => {
+  try {
+    const employee = await Employee.find()
+    return res.status(201).send(employee)
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+}
+
+const getEmployeeById = async (req, res) => {}
+
+module.exports = {
+  createEmployee,
+  getEmployees
+}
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,10 @@
 const { Router } = require('express')
-// const controllers = require('../controllers')
+const controllers = require('../controllers')
 const router = Router()
 
 router.get('/', (req, res) => res.send('Employees init!'))
+
+router.post('/employee', controllers.createEmployee)
+router.get('/employee', controllers.getEmployees)
 
 module.exports = router
