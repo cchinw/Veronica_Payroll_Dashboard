d8f92000773271c3a4786d590043a56db5deef42
Chinwendu <chinwenduukoha@gmail.com>
2022-06-18 19:20:35 -0400

    pinged server

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const EmployeeSchema = require('./Employee')
 
-const Employee = mongoose.model('employee', EmployeeSchema)
+const Employee = mongoose.model('employees', EmployeeSchema)
 
 module.exports = {
   Employee
